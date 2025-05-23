8e89fc817794c1973282b8461cbc571ba041bae4
SiphiweRadebe <siphiwemoeketsi1@gmail.com>
2025-05-23 07:03:23 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MaintenancePortal.Models;
+using System.Collections.Generic;
+
+namespace MaintenancePortal.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<School> Schools { get; set; }
+    }
+}
---
+++ Models/School.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MaintenancePortal.Models
+{
+    public class School
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string SchoolName { get; set; }
+
+        [Required]
+        public string EMISNumber { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string PrincipalFirstName { get; set; }
+
+        [Required]
+        public string PrincipalLastName { get; set; }
+
+        [EmailAddress]
+        public string PrincipalEmail { get; set; }
+
+        [Phone]
+        public string PrincipalPhone { get; set; }
+
+        [Required]
+        public string DistrictName { get; set; }
+    }
+}
